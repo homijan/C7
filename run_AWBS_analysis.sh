e8c8d3de62534323d1642652b01277b63edceb70
Milan Holec <holec@st-huckel.celia.u-bordeaux1.fr>
2018-03-01 14:20:44 +0100

    First analysis of fully-implicit results. Added OSHUN Spitzer simulation data.

--- run_AWBS_analysis.sh
+++ run_AWBS_analysis.sh
@@ -14,13 +14,13 @@ TMIN=100
 TGRAD=180
 XPOINT=0.046775 # in cm
 
-ZBAR=1
+#ZBAR=1
 #ZBAR=2
 #ZBAR=5
 #ZBAR=10
 #ZBAR=20
 #ZBAR=50
-#ZBAR=100
+ZBAR=100
 
 MINGSAFE=1000
 MINGIMPL=35
@@ -29,8 +29,10 @@ L=0.1
 PROBLEM=5
 
 #if false; then
-#NI=2e25 # Zbar = 100 -> Kn 1e-10
-NI=1e29 # Zbar = 1 -> Kn 1e-10
+NI=2e25 # Zbar = 100 -> Kn 1e-10
+if [ $ZBAR -le 10 ] ; then
+   NI=1e29 # Zbar = 1 -> Kn 1e-10
+fi
 ## Nonlocal solution very well corresponding to Pascal's solution with Aladin.
 ## P1 closure.
 ## C7*
@@ -48,8 +50,10 @@ cd ../..
 #fi
 
 #if false; then
-#NI=2e17 # Zbar = 100 -> Kn 1e-2
-NI=1e21 # Zbar = 1 -> Kn 1e-2
+NI=2e18 # Zbar = 100 -> Kn 1e-3
+if [ $ZBAR -le 10 ] ; then
+   NI=1e21 # Zbar = 1 -> Kn 1e-2
+fi
 mpirun -np $NPROC C7 -p $PROBLEM -m data/segment01.mesh -rs $RS -tf 0.0 -ok $F1ORDER -ot $F0ORDER -no-vis -fa -print -Tmax $TMAX -Tmin $TMIN -Tgrad $TGRAD -Z $ZBAR -cl $CL -ni $NI -L $L -xp $XPOINT -minG $MINGIMPL -s 4 -cfl 1e10
 cp results/tmp/C7_1_profiles.* results/fe_analysis/Emimic_data/
 cp results/tmp/C7_1_fe_point.txt results/fe_analysis/Emimic_data/fe_point_Emimic.txt
@@ -61,8 +65,10 @@ cd ../..
 
 #if false; then
 ## Highest Kn limit to compute.
-#NI=2e20 # Zbar = 100 -> Kn 1e-5
-NI=1e22 # Zbar = 1 -> Kn 1e-3
+NI=2e20 # Zbar = 100 -> Kn 1e-5
+if [ $ZBAR -le 10 ] ; then
+   NI=1e22 # Zbar = 1 -> Kn 1e-3
+fi
 mpirun -np $NPROC C7 -p $PROBLEM -m data/segment01.mesh -rs $RS -tf 0.0 -ok $F1ORDER -ot $F0ORDER -no-vis -fa -print -Tmax $TMAX -Tmin $TMIN -Tgrad $TGRAD -Z $ZBAR -cl $CL -ni $NI -L $L -xp $XPOINT -minG $MINGIMPL -s 4 -cfl 1e10 -S0 1.0 -E0 1.0
 cp results/tmp/C7_1_profiles.* results/fe_analysis/Emimic_data/
 cp results/tmp/C7_1_fe_point.txt results/fe_analysis/Emimic_data/fe_point_Emimic.txt
@@ -80,7 +86,7 @@ cd ../..
 ## A pure diffusion case. 
 ## Converged numerical flux from -minG 200. Err 1e-5 -minG 50.
 
-RS=6
+RS=8
 F1ORDER=3
 F0ORDER=2
 MING=25
@@ -98,12 +104,15 @@ PROBLEM=5
 #if false ; then
 #NI=7.044e25 # Zbar = 100, Kn = 1e-10
 #NI=7.044e20 # Zbar = 100, Kn = 1e-5
-#NI=7.044e18 # Zbar = 100, Kn = 1e-3
-NI=3.522e22 # Zbar = 1, Kn = 1e-3
+NI=7.044e18 # Zbar = 100, Kn = 1e-3
+if [ $ZBAR -le 10 ] ; then
+   #NI=3.522e22 # Zbar = 1, Kn = 1e-3
+   NI=3.522e21 # Zbar = 1, Kn = 1e-2
+fi
 mpirun -np $NPROC C7 -p $PROBLEM -m data/segment01.mesh -rs $RS -tf 0.0 -ok $F1ORDER -ot $F0ORDER -no-vis -fa -print -Tmax $TMAX -Tmin $TMIN -Tgrad $TGRAD -Z $ZBAR -cl $CL -ni $NI -L $L -xp $XPOINT -minG $MINGIMPL -s 4 -cfl 1e10 -S0 1.0 -E0 1.0
 cp results/tmp/C7_1_profiles.* results/fe_analysis/Emimic_data/
 cp results/tmp/C7_1_fe_point.txt results/fe_analysis/Emimic_data/fe_point_Emimic.txt
 
 cd results/fe_analysis
-python C7_AWBS_SH_analysis.py -N $NPROC -Z $ZBAR -cl $CL -n $NI -xp $XPOINT --Emimic --labelEmimic 'C7*' --AWBSoriginal
+python C7_AWBS_SH_analysis.py -N $NPROC -Z $ZBAR -cl $CL -n $NI -xp $XPOINT --Emimic --labelEmimic 'C7impl' --AWBSoriginal
 #fi
