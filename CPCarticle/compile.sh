56c781d6c82dd922ae0398452ca208bac4575c55
milan_holec <homijan@gmail.com>
2018-06-14 21:54:08 +0200

    Equations in AWBS diffusive completed.

--- CPCarticle/compile.sh
+++ CPCarticle/compile.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-#pdflatex C7.tex
-#bibtex C7
-#pdflatex C7.tex
-#pdflatex C7.tex
+pdflatex C7.tex
+bibtex C7
+pdflatex C7.tex
+pdflatex C7.tex
 pdflatex AWBS.tex
 bibtex AWBS
 pdflatex AWBS.tex
