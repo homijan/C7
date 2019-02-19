76ed4a7f1323cc06f00cab223073101e3b1bf0f6
milan_holec <homijan@gmail.com>
2019-02-19 02:59:59 +0100

    CSE2019 presentation - dobry polotovar

--- CPCarticle/presentation/compile.sh
+++ CPCarticle/presentation/compile.sh
@@ -2,11 +2,13 @@
 rm -r build
 mkdir build && cd build
 
-cp ../nonlocalMHD.tex document.tex
+#cp ../nonlocalMHD.tex document.tex
+cp ../CSE2019nonlocalMHD.tex document.tex
 
 pdflatex document.tex
 pdflatex document.tex
 
-mv document.pdf ../nonlocalMHD.pdf
+#mv document.pdf ../nonlocalMHD.pdf
+mv document.pdf ../CSE2019nonlocalMHD.pdf
 cd ..
 rm -r build
